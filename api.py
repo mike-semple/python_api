21bd35a386dfc3ae459d4c0e4759fa901633f6fc
msemple1111 <mwsemple@me.com>
2015-01-25 11:36:30 +0000

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,78 @@
-#h
+#school Computers Api V1.1 
+#Written in python by Mike Semple
+import json
+import bottle
+from bottle import route, run, request, abort
+from pymongo import Connection
+ 
+connection = Connection('localhost', 27017)
+db = connection.mydatabase
+@route('/api/v1.1/14be4a968a6c807ba132ab6a', method='PUT')
+def put_document_old():
+    data = request.body.readline()
+    if not data:
+        abort(400, 'No data received')
+    entity = json.loads(data)
+    if not entity.has_key('_id'):
+        abort(400, 'No _id specified')
+    try:
+        db['computers'].save(entity)
+    except ValidationError as ve:
+        abort(400, str(ve))
+        
+@route('/api/v1.1/reserve', method='PUT')
+def reserve():
+    data = request.body.readline()
+    if not data:
+        abort(400, 'No data received')
+    entity = json.loads(data)
+    if not entity.has_key('_id'):
+        abort(400, 'No _id specified')
+    try:
+      rm_no = entity['_id']
+      rm = db['computers'].find_one({'_id':rm_no})
+      old_rm = rm['no_pcs']
+      if old_rm > 0:
+        new_rm = old_rm - 1
+        rm['no_pcs'] = new_rm
+        db['computers'].save(rm)
+        return rm
+      else:
+        abort(400, 'Not Enogh PCs')
+        
+    except ValidationError as ve:
+        abort(400, str(ve))
+     
+@route('/api/v1.1/cancel', method='PUT')
+def cancel():
+    data = request.body.readline()
+    if not data:
+        abort(400, 'No data received')
+    entity = json.loads(data)
+    if not entity.has_key('_id'):
+        abort(400, 'No _id specified')
+    try:
+      rm_no = entity['_id']
+      rm = db['computers'].find_one({'_id':rm_no})
+      old_rm = rm['no_pcs']
+      max_pc = rm['max_pcs']
+      if old_rm < max_pc:
+        new_rm = old_rm + 1
+        rm['no_pcs'] = new_rm
+        db['computers'].save(rm)
+        return rm
+      else:
+        abort(400, 'Too Many PCs')
+        
+    except ValidationError as ve:
+        abort(400, str(ve))
+
+@route('/api/v1.1/get/:id', method='GET')
+def get_entity(id):
+    entity = db['computers'].find_one({'_id':id})
+    if not entity:
+        abort(404, 'No document with id %s' % id)
+    return entity
+  
+run(host='0.0.0.0', port=8080)
+
